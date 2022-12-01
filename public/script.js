8e62b8c826d3a91673e361d8cf323e78b2057249
Sebastian <zzevazzz_1@hotmail.com>
2022-12-01 13:12:20 -0300

    menu responsive commit

--- public/script.js
+++ public/script.js
@@ -33,3 +33,13 @@ lista.appendChild(li)
 
 }
 DataGit();
+
+
+function responsiveMenu() {
+    var x = document.getElementById("nav");
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
+}
