6519b6d1a4793d136952adc7f5ac099830bc96f3
Sebastian <zzevazzz_1@hotmail.com>
2022-12-01 23:36:21 -0300

    Actualizacion Descripcion commit

--- public/script.js
+++ public/script.js
@@ -1,3 +1,11 @@
+function responsiveMenu() {
+    var x = document.getElementById("nav");
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
+}
 
 
 const lista = document.getElementById('uli');
@@ -35,11 +43,3 @@ lista.appendChild(li)
 DataGit();
 
 
-function responsiveMenu() {
-    var x = document.getElementById("nav");
-    if (x.className === "") {
-        x.className = "responsive";
-    } else {
-        x.className = "";
-    }
-}
